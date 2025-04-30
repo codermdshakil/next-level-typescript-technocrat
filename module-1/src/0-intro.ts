bb3e342d06e0a051d03f737d8530ceaec4c469c0
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-30 21:34:51 +0600

    Learn : What is typescript? why should i need it and what is the benefits of ts

---
+++ module-1/src/0-intro.ts
@@ -0,0 +1,41 @@
+// ## What is TypeScript?
+
+// TypeScript is a Object Oriented Programming Language that build with JavaScript with Extra Features.
+
+// ### Lackings in JavaScript
+
+// - Dynamically Typed Languages
+// - in large application very difficult to maintain codebase
+// - in large application hard to find bugs
+// - Catch errors only runtime
+// - Need ES3 supports in the older browser but you like modern syntax you can’t switch but in typescript you can switch any version
+
+// ### Benefits of TypeScript
+
+// - Supports Older Versions
+// - Type Safety
+// - Increase your productivity
+// - Less bugs and less testing
+// - We can use JS types in TS
+//     - Number
+//     - String
+//     - Boolean
+//     - Null
+//     - Array
+//     - Object
+//     - Symbol
+// - TypeScript Own Type
+//     - Void
+//     - Interface
+//     - Array
+//     - Tuple
+//     - Enum
+//     - Union
+//     - Intersection
+
+// ## Drawbacks of using TypeScript
+
+// - Type Complexity
+// - Limited Library Support
+// - Over Engineering
+// - Migration Challenges
