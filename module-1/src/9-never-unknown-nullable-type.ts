6d18bd33a2d40a3955c9804899cd9f2ddcdf06bc
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-01 22:54:15 +0600

    Learn : any vs unknown

---
+++ module-1/src/9-never-unknown-nullable-type.ts
@@ -0,0 +1,53 @@
+{
+    /**
+     * Never Unknown Nullable-type 
+     * 
+     * 
+     * Any VS unknown
+     * 
+     * any = 🔓 Open box — you can do anything with it, even dangerous things.
+     * unknown = 🔒 Locked box — you must open it carefully (check the type first).
+     * 
+    */
+
+    // nullable types
+    const searchName = (value:string | null) => {
+        if(typeof value === 'string'){
+            console.log('Searching...');
+        }
+        else{
+            console.log('there is nothing to search');
+        }
+    }
+
+    // searchName(null);
+
+    // unknown type
+    // - unknonw value must check first then use
+    const getSpeedInMeterPerSec = (value:unknown) => {
+        if(typeof value === 'number'){
+            const convertedSpeed = (value * 1000) / 3600;
+            console.log(`The converted speed is ${convertedSpeed} m/s`);
+            return convertedSpeed;
+        }
+        else if(typeof value === 'string'){
+            const speed =parseFloat(value.split(' ')[0]) ;
+            const convertedSpeed = (speed * 1000) / 3600;
+            console.log(`The converted speed is ${convertedSpeed} m/s`);
+            return convertedSpeed;
+        }
+        else{
+            console.log('Inter number input');
+        }
+    };
+    
+    const res1 = getSpeedInMeterPerSec('1000 kmh^-1');
+    console.log(res1);
+
+
+
+    
+
+
+
+}
