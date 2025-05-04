647f297a2e861e0b98f33101b9cd332966270beb
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-04 15:48:37 +0600

    Completed: Task - 1

---
+++ module-2/tasks/task-1.ts
@@ -0,0 +1,7 @@
+{
+    // Task 1: Basic Data Types and First Program 
+    const welcome: string = "Hello World, I will complete this course successfully and become a Next level Web Developer!";
+    console.log(welcome);
+    
+
+}
