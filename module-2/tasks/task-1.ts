3c99ac2aa07403f9c8e29ca3ab88520d11b2ea40
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-04 15:56:29 +0600

    Completed: Task - 3

--- module-2/tasks/task-1.ts
+++ module-2/tasks/task-1.ts
@@ -5,23 +5,40 @@
 
     // Task 2: Functions, Optional, and Literal Types
 
-    interface Person {
+    interface User {
         name:string;
         age:number;
         role:"admin"| "user"| "guest";
     };
 
-    const person1  : Person = {
+    const person1  : User = {
         name:'Shakil Ahmed',
         age:20,
         role: 'admin'
     };
-    
-    const printValues = (person: Person) => {
+
+    const printValues = (person: User) => {
         console.log(`My name is ${person.name}. My age is ${person.age} and I am ${person.role}`) ; 
     };
 
-    // printValues(person1);
+    // printValues(person1);]
+
+    // Task 3: Object Types, Type Alias, & Literal Types
+
+    type Person = {
+        name:string;
+        address:string;
+        age:number;
+        hairAndEyeColor:string;
+        IncomeAndExpence: string;
+        hobbies:string;
+        familyMembers: number;
+        job:string;
+        skills: string[];
+        MaritalStatus: boolean;
+        friends:string[]
+    }
+
 
 
 }
