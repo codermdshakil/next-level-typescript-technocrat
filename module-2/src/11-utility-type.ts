4d086e778385e83b54a2e3834c15ab97b7371e86
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-04 15:45:20 +0600

    * Topic: Utility Type      *      * | Utility Type     | Purpose                           |      * | ---------------  | --------------------------------- |      * | Partial<T>       | All properties optional           |      * | Required<T>      | All properties required           |      * | Readonly<T>      | All properties readonly           |      * | Pick<T, K>       | Select only K keys                |      * | Omit<T, K>       | Remove keys K                     |      * | Record<K, T>     | Object with keys K and values T   |      * | Exclude<T, U>    | Remove U from T (in unions)       |      * | Extract<T, U>    | Keep only shared types            |      * | NonNullable<T>   | Remove  and      |      * | ReturnType<T>    | Get return type of a function     |      * | Parameters<T>    | Get parameter types of a function |      *      *

---
+++ module-2/src/11-utility-type.ts
@@ -0,0 +1,24 @@
+{
+    /**
+     * Topic: Utility Type
+     * 
+     * | Utility Type     | Purpose                           |
+     * | ---------------  | --------------------------------- |
+     * | Partial<T>       | All properties optional           |
+     * | Required<T>      | All properties required           |
+     * | Readonly<T>      | All properties readonly           |
+     * | Pick<T, K>       | Select only K keys                |
+     * | Omit<T, K>       | Remove keys K                     |
+     * | Record<K, T>     | Object with keys K and values T   |
+     * | Exclude<T, U>    | Remove U from T (in unions)       |
+     * | Extract<T, U>    | Keep only shared types            |
+     * | NonNullable<T>   | Remove `null` and `undefined`     |
+     * | ReturnType<T>    | Get return type of a function     |
+     * | Parameters<T>    | Get parameter types of a function |
+     * 
+     * 
+    */
+
+
+
+}
